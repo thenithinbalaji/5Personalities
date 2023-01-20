9b5c73f6bb2c55e9fc79d1cd087f580fdb300303
thenithinbalaji <thenithinbalaji@gmail.com>
2023-01-20 07:33:41 +0530

    converted intendation to spaces

--- app.py
+++ app.py
@@ -14,10 +14,10 @@ data = pd.read_csv("dataset.csv")
 array = data.values
 
 for i in range(len(array)):
-	if array[i][0] == "Male":
-		array[i][0] = 1
-	else:
-		array[i][0] = 0
+    if array[i][0] == "Male":
+        array[i][0] = 1
+    else:
+        array[i][0] = 0
 
 df = pd.DataFrame(array)
 
@@ -29,26 +29,26 @@ train_y = temp.values
 train_y = temp.values
 
 for i in range(len(train_y)):
-	train_y[i] = str(train_y[i])
+    train_y[i] = str(train_y[i])
 
-mul_lr = linear_model.LogisticRegression(multi_class="multinomial",
-                                         solver="newton-cg",
-                                         max_iter=1000)
+mul_lr = linear_model.LogisticRegression(
+    multi_class="multinomial", solver="newton-cg", max_iter=1000
+)
 mul_lr.fit(mainarray, train_y)
 #####################################################
 
 
 @app.route("/", methods=["POST", "GET"])
 def home():
-	if request.method == "GET":
-		return render_template("index.html")
+    if request.method == "GET":
+        return render_template("index.html")
 
-	else:
-		age = int(request.form["age"])
-		if age < 17:
-			age = 17
-		elif age > 28:
-			age = 28
+    else:
+        age = int(request.form["age"])
+        if age < 17:
+            age = 17
+        elif age > 28:
+            age = 28
 
         inputdata = [
             [
@@ -62,48 +62,48 @@ def home():
             ]
         ]
 
-		for i in range(len(inputdata)):
-			if inputdata[i][0] == "Male":
-				inputdata[i][0] = 1
-			else:
-				inputdata[i][0] = 0
+        for i in range(len(inputdata)):
+            if inputdata[i][0] == "Male":
+                inputdata[i][0] = 1
+            else:
+                inputdata[i][0] = 0
 
-		df1 = pd.DataFrame(inputdata)
-		testdf = df1[[0, 1, 2, 3, 4, 5, 6]]
-		maintestarray = testdf.values
+        df1 = pd.DataFrame(inputdata)
+        testdf = df1[[0, 1, 2, 3, 4, 5, 6]]
+        maintestarray = testdf.values
 
-		y_pred = mul_lr.predict(maintestarray)
-		for i in range(len(y_pred)):
-			y_pred[i] = str((y_pred[i]))
-		DF = pd.DataFrame(y_pred, columns=["Predicted Personality"])
-		DF.index = DF.index + 1
-		DF.index.names = ["Person No"]
+        y_pred = mul_lr.predict(maintestarray)
+        for i in range(len(y_pred)):
+            y_pred[i] = str((y_pred[i]))
+        DF = pd.DataFrame(y_pred, columns=["Predicted Personality"])
+        DF.index = DF.index + 1
+        DF.index.names = ["Person No"]
 
-		return render_template("result.html",
-		                       per=DF["Predicted Personality"].tolist()[0])
+        return render_template(
+            "result.html", per=DF["Predicted Personality"].tolist()[0]
+        )
 
 
 @app.route("/learn")
 def learn():
-	return render_template("learn.html")
+    return render_template("learn.html")
 
 
 @app.route("/working")
 def working():
-	return render_template("working.html")
+    return render_template("working.html")
 
 
 # Handling error 404
 @app.errorhandler(404)
 def not_found_error(error):
-	return render_template("error.html", code=404, text="Page Not Found"), 404
+    return render_template("error.html", code=404, text="Page Not Found"), 404
 
 
 # Handling error 500
 @app.errorhandler(500)
 def internal_error(error):
-	return render_template("error.html", code=500,
-	                       text="Internal Server Error"), 500
+    return render_template("error.html", code=500, text="Internal Server Error"), 500
 
 
 if __name__ == "__main__":
